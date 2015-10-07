4c944cca80c9dbda45d85fe157f6395477c16a0f
Samuel Jenness <sjenness@uw.edu>
2015-10-07 15:25:00 -0700

    Prevent error in delete_vertices

--- tergmLite/R/update.network.R
+++ tergmLite/R/update.network.R
@@ -8,18 +8,21 @@
 #' @export
 delete_vertices <- function(el, vid) {
 
+  if (length(vid) == 0) {
+    return(el)
+  }
   el.rows.to.del <- which(el[, 1] %in% vid | el[, 2] %in% vid)
+  if (length(el.rows.to.del) == 0) {
+    return(el)
+  }
   new.el <- el[-el.rows.to.del, ]
 
   elv <- as.vector(new.el)
   shifted.elv <- vapply(1:length(elv),
                         function(x) elv[x] - sum(elv[x] > vid), FUN.VALUE = integer(1))
 
-  # vid <- sort(vid)
-  # for (i in 1:length(vid)) {
-  #   new.el[new.el > vid[i]] <- new.el[new.el > vid[i]] - 1
-  # }
+  new.el <- matrix(shifted.elv, ncol = 2)
+  attributes(new.el)$n <- attributes(el)$n - length(vid)
 
-  return(matrix(shifted.elv, ncol = 2))
-  # return(new.el)
+  return(new.el)
 }
