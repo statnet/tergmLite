ec80271625dd4e926d07002ee02d6791dbd457bc
Samuel Jenness <sjenness@uw.edu>
2015-10-08 16:49:28 -0700

    Fix bug in delete_vertices

--- tergmLite/R/update.network.R
+++ tergmLite/R/update.network.R
@@ -8,21 +8,20 @@
 #' @export
 delete_vertices <- function(el, vid) {
 
-  if (length(vid) == 0) {
-    return(el)
-  }
-  el.rows.to.del <- which(el[, 1] %in% vid | el[, 2] %in% vid)
-  if (length(el.rows.to.del) == 0) {
-    return(el)
-  }
-  new.el <- el[-el.rows.to.del, ]
+  new.el <- el
+  if (length(vid) > 0) {
+    el.rows.to.del <- which(el[, 1] %in% vid | el[, 2] %in% vid)
+    if (length(el.rows.to.del) > 0) {
+      new.el <- el[-el.rows.to.del, ]
 
-  elv <- as.vector(new.el)
-  shifted.elv <- vapply(1:length(elv),
-                        function(x) elv[x] - sum(elv[x] > vid), FUN.VALUE = integer(1))
+      elv <- as.vector(new.el)
+      shifted.elv <- vapply(1:length(elv),
+                            function(x) elv[x] - sum(elv[x] > vid), FUN.VALUE = integer(1))
 
-  new.el <- matrix(shifted.elv, ncol = 2)
-  attributes(new.el)$n <- attributes(el)$n - length(vid)
+      new.el <- matrix(shifted.elv, ncol = 2)
+    }
+    attributes(new.el)$n <- attributes(el)$n - length(vid)
+  }
 
   return(new.el)
 }
