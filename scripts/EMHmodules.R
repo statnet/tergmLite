b29485d1c4b7a6057f8755f9a3009f65c6856c3f
Samuel Jenness <sjenness@uw.edu>
2015-10-12 07:40:55 -0700

    Add update_nwp_lim function for edges NW model

--- scripts/EMHmodules.R
+++ scripts/EMHmodules.R
@@ -334,6 +334,61 @@ update_nwp <- function(dat, at) {
 
 }
 
+update_nwp_lim <- function(dat, at) {
+
+  mf <- dat$p$model.form
+  md <- dat$p$model.diss
+  mhf <- dat$p$MHproposal.form
+  mhd <- dat$p$MHproposal.diss
+
+  n <- attributes(dat$el)$n
+  maxdyads <- choose(n, 2)
+
+  ## 1. Update model.form ##
+
+  # edges
+  # inputs <- c(0, 1, 0) # not changed
+  mf$terms[[1]]$maxval <- maxdyads
+
+  # nodematch
+  nodecov <- dat$attr$male
+  u <- sort(unique(nodecov))
+  nodecov <- match(nodecov, u, nomatch = length(u) + 1)
+  inputs <- nodecov
+  mf$terms[[2]]$inputs <- c(0, 1, length(inputs), inputs)
+
+  ## Update combined maxval here
+  mf$maxval <- c(maxdyads, Inf)
+
+
+  ## 2. Update model.diss ##
+  md$terms[[1]]$maxval <- maxdyads
+  md$maxval <- maxdyads
+
+
+  ## 3. Update MHproposal.form ##
+  mhf$arguments$constraints$bd$attribs <-
+    matrix(rep(mhf$arguments$constraints$bd$attribs[1], n), ncol = 1)
+  mhf$arguments$constraints$bd$maxout <-
+    matrix(rep(mhf$arguments$constraints$bd$maxout[1], n), ncol = 1)
+  mhf$arguments$constraints$bd$maxin <- matrix(rep(n, n), ncol = 1)
+  mhf$arguments$constraints$bd$minout <-
+    mhf$arguments$constraints$bd$minin <- matrix(rep(0, n), ncol = 1)
+
+
+  ## 4. Update MHproposal.diss ##
+  mhd$arguments$constraints$bd <- mhf$arguments$constraints$bd
+
+
+  ## 5. Output ##
+  p <- list(model.form = mf, model.diss = md,
+            MHproposal.form = mhf, MHproposal.diss = mhd)
+  dat$p <- p
+
+  return(dat)
+
+}
+
 
 new.simnet.hiv <- function(dat, at) {
 
