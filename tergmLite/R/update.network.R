0254a52e0d164a75b93d14f71612ac598ddebb54
Samuel Jenness <sjenness@uw.edu>
2015-10-16 12:04:51 -0700

    Fix bug in delete_vertices

--- tergmLite/R/update.network.R
+++ tergmLite/R/update.network.R
@@ -13,13 +13,11 @@ delete_vertices <- function(el, vid) {
     el.rows.to.del <- which(el[, 1] %in% vid | el[, 2] %in% vid)
     if (length(el.rows.to.del) > 0) {
       new.el <- el[-el.rows.to.del, ]
-
-      elv <- as.vector(new.el)
-      shifted.elv <- vapply(1:length(elv),
-                            function(x) elv[x] - sum(elv[x] > vid), FUN.VALUE = integer(1))
-
-      new.el <- matrix(shifted.elv, ncol = 2)
     }
+    elv <- as.vector(new.el)
+    shifted.elv <- vapply(1:length(elv),
+                          function(x) elv[x] - sum(elv[x] > vid), FUN.VALUE = integer(1))
+    new.el <- matrix(shifted.elv, ncol = 2)
     attributes(new.el)$n <- attributes(el)$n - length(vid)
   }
 
