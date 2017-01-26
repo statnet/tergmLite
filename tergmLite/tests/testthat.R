78dda4f8a40f5b2e38b6a0039ee82a69ff816b04
smjenness <sjenness@uw.edu>
2017-01-26 11:49:47 -0500

    Start tests folder

---
+++ tergmLite/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(tergmLite)
+
+test_check("tergmLite")
---
+++ tergmLite/tests/testthat/tests.R
@@ -0,0 +1,59 @@
+
+test_that("concurrent", {
+
+  nw <- network.initialize(100, directed = FALSE)
+
+  est <- netest(nw = nw,
+                formation = ~edges + concurrent,
+                target.stats = c(50, 25),
+                coef.diss = dissolution_coefs(~offset(edges), duration = 100))
+
+  p <- stergm_prep(nw,
+                   est$formation,
+                   est$coef.diss$dissolution,
+                   est$coef.form,
+                   est$coef.diss$coef.adj,
+                   est$constraints)
+
+  dat <- list()
+  dat$p <- dat$el <- dat$attr <- dat$nwparam <- list()
+  dat$p[[1]] <- p
+  dat$el[[1]] <- as.edgelist(simulate(est$fit))
+  dat$nwparam[[1]] <- est
+
+  dat <- updateModelTermInputs(dat, network = 1)
+
+  expect_identical(dat$p[[1]], p)
+
+})
+
+test_that("concurrent_by", {
+
+  nw <- network.initialize(100, directed = FALSE)
+  nw <- set.vertex.attribute(nw, "riskg", rbinom(100, 1, 0.5))
+
+  est <- netest(nw = nw,
+                formation = ~edges + concurrent(by = "riskg"),
+                target.stats = c(50, 30, 20),
+                coef.diss = dissolution_coefs(~offset(edges), duration = 100))
+
+  p <- stergm_prep(nw,
+                   est$formation,
+                   est$coef.diss$dissolution,
+                   est$coef.form,
+                   est$coef.diss$coef.adj,
+                   est$constraints)
+
+  dat <- list()
+  dat$p <- dat$el <- dat$attr <- dat$nwparam <- list()
+  dat$p[[1]] <- p
+  dat$el[[1]] <- as.edgelist(simulate(est$fit))
+  dat$attr$riskg <- nw %v% "riskg"
+  dat$nwparam[[1]] <- est
+
+  dat <- updateModelTermInputs(dat, network = 1)
+
+  expect_identical(dat$p[[1]], p)
+
+})
+
