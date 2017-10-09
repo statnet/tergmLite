ac5c6b4202427523f4941ff38caa0a93186968ea
smjenness <smjenness@gmail.com>
2017-10-09 11:19:46 -0400

    Add test for degrange term
    For #11

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -59,7 +59,34 @@ test_that("concurrent_by", {
 
 })
 
+test_that("degrange", {
 
+  library("EpiModel")
+  nw <- network.initialize(100, directed = FALSE)
+
+  est <- netest(nw = nw,
+                formation = ~edges + degrange(from = 4),
+                target.stats = c(50, 0),
+                coef.diss = dissolution_coefs(~offset(edges), duration = 100))
+
+  p <- stergm_prep(nw,
+                   est$formation,
+                   est$coef.diss$dissolution,
+                   est$coef.form,
+                   est$coef.diss$coef.adj,
+                   est$constraints)
+
+  dat <- list()
+  dat$p <- dat$el <- dat$attr <- dat$nwparam <- list()
+  dat$p[[1]] <- p
+  dat$el[[1]] <- as.edgelist(simulate(est$fit))
+  dat$nwparam[[1]] <- est
+
+  dat <- updateModelTermInputs(dat, network = 1)
+
+  expect_identical(dat$p[[1]], p)
+
+})
 
 test_that("get_formula_term...", {
 
