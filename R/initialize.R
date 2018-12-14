0d747cb951769f9d21da596f441542f34c3217df
smjenness <smjenness@gmail.com>
2018-12-14 16:11:21 -0500

    Fix bug in init_tergmLite

--- R/initialize.R
+++ R/initialize.R
@@ -115,7 +115,7 @@ ergm_prep <- function(nw,
 #'
 init_tergmLite <- function(dat) {
 
-  num_nw <- ifelse(any(class(dat$nw) == "network"), 1, length(dat$nw))
+  num_nw <- ifelse(inherits(dat$nw, "network"), 1, length(dat$nw))
 
   dat$el <- list()
   dat$p <- list()
