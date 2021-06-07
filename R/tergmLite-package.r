4b733cf35fa602d290f9d6d7d0d3ca5a11ef6c35
Samuel Jenness <smjenness@gmail.com>
2021-06-07 10:11:32 -0400

    Bump version/date

--- R/tergmLite-package.r
+++ R/tergmLite-package.r
@@ -4,8 +4,8 @@
 #' \tabular{ll}{
 #'    Package: \tab tergmLite\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 2.2.1\cr
-#'    Date: \tab 2020-10-06\cr
+#'    Version: \tab 2.5.0\cr
+#'    Date: \tab 2021-06-07\cr
 #'    License: \tab GPL-3\cr
 #'    LazyLoad: \tab yes\cr
 #' }
